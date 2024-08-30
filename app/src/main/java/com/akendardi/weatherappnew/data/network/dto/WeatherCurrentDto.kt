46e9733cd07f7f1098b3dfb59bfdd4f223eea5b6
akendardi <148619693+akendardi@users.noreply.github.com>
2024-08-30 14:44:59 +0500

    Create internet work

---
+++ app/src/main/java/com/akendardi/weatherappnew/data/network/dto/WeatherCurrentDto.kt
@@ -0,0 +1,7 @@
+package com.akendardi.weatherappnew.data.network.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherCurrentDto(
+    @SerializedName("current") val current: WeatherDto
+)
